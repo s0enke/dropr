ca30e57cd74091c3edaa7f761378783fb9e24c67
Boris Erdmann <boris@jimdo.com>
2007-11-17 22:42:05 +0000

    git-svn-id: https://www.dropr.org/svn/trunk@70 f77b8f78-39ad-454d-97bc-04c3ead53514

--- tests/client/ClientApiTest.php
+++ tests/client/ClientApiTest.php
@@ -16,7 +16,7 @@ class ClientApiTest extends PHPUnit_Framework_TestCase
 	{
         require '../../client/classes/autoload.php';		
 
-        $this->storage = pmq_Client_Storage_Abstract::factory('filesystem', '/tmp/myqueue');
+        $this->storage = pmq_Client_Storage_Abstract::factory('filesystem', '/tmp/myqueue2');
         $this->queue = new pmq_Client($this->storage);
 	}
 

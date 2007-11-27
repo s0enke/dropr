aa5a89c79c44dc097c1606e1f7f14830ac301575
Soenke Ruempler <soenke@jimdo.com>
2007-11-27 22:25:32 +0000

    git-svn-id: https://www.dropr.org/svn/trunk@126 f77b8f78-39ad-454d-97bc-04c3ead53514

--- server/classes/autoload.php
+++ server/classes/autoload.php
@@ -12,6 +12,9 @@ set_include_path(get_include_path() . PATH_SEPARATOR . realpath(dirname(__FILE__
 
 function pmq_Client_Autoload($className)
 {
+  	if (strpos($className, 'pmq_') !== 0) {
+            return;
+        }
   	$file = substr(str_replace('_', '/', $className), 4) . '.php';
     
   	require realpath(dirname(__FILE__)) . '/' . $file;

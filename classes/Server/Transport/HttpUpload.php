c9a8e919e03a4ecd7748158c4a8d9a8dc7672fa2
Soenke Ruempler <soenke@jimdo.com>
2008-08-16 14:03:41 +0000

    git-svn-id: https://www.dropr.org/svn/trunk@178 f77b8f78-39ad-454d-97bc-04c3ead53514

--- classes/Server/Transport/HttpUpload.php
+++ classes/Server/Transport/HttpUpload.php
@@ -1,4 +1,46 @@
 <?php
+/**
+ * dropr
+ *
+ * Copyright (c) 2007 - 2008 by the dropr project https://www.dropr.org/
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of dropr nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    dropr
+ * @author     Soenke Ruempler <soenke@jimdo.com>
+ * @author     Boris Erdmann <boris@jimdo.com>
+ * @copyright  2007-2008 Soenke Ruempler, Boris Erdmann
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ */
+
 class dropr_Server_Transport_HttpUpload extends dropr_Server_Transport_Abstract 
 {
     
@@ -16,8 +58,7 @@ class dropr_Server_Transport_HttpUpload extends dropr_Server_Transport_Abstract
         
         // xxx check the check
         if (!isset($_POST['metaData']) || !is_string($_POST['metaData']) || (!$metadata = unserialize($_POST['metaData']))) {
-            error_log(print_r($_POST['metaData'], true));
-        	throw new Exception("No client metadata set!");
+        	throw new Exception("No client metadata set - do you have magic_quotes enabled?");
         }
         
         #print_r($_FILES);exit;
